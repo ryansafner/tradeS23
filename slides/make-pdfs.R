854a946e3ecade456a77094b6f4c1184acb2cf21
Ryan Safner <ryansafner@gmail.com>
2023-01-24 11:00:47 -0500

    Syllabus pdf

--- slides/make-pdfs.R
+++ slides/make-pdfs.R
@@ -2,13 +2,13 @@
 rmarkdown::render("slides/1.1-slides.Rmd")
 
 # make-pdfs
-pagedown::chrome_print(input = "https://microf22.classes.ryansafner.com/slides/4.5-slides.html",
-                       output = "slides/4.5-slides.pdf",
+pagedown::chrome_print(input = "https://tradeS23.classes.ryansafner.com/slides/1.1-slides.html",
+                       output = "slides/1.1-slides.pdf",
                        timeout = 300)
 
 # create png of first slide
-pagedown::chrome_print(input = "https://microf22.classes.ryansafner.com/slides/4.5-slides.html",
-                       output = "slides/4.5-slides.png",
+pagedown::chrome_print(input = "https://tradeS23.classes.ryansafner.com/slides/1.1-slides.html",
+                       output = "slides/1.1-slides.png",
                        selector = "div.remark-slide",
                        format = "png",
                        timeout = 300)
