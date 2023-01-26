af98c5d2790ff9175ab71080ac178007961717b9
Ryan Safner <ryansafner@gmail.com>
2023-01-26 11:24:32 -0500

    1.2 pdf

--- slides/make-pdfs.R
+++ slides/make-pdfs.R
@@ -2,13 +2,13 @@
 rmarkdown::render("slides/1.1-slides.Rmd")
 
 # make-pdfs
-pagedown::chrome_print(input = "https://tradeS23.classes.ryansafner.com/slides/1.1-slides.html",
-                       output = "slides/1.1-slides.pdf",
+pagedown::chrome_print(input = "https://tradeS23.classes.ryansafner.com/slides/1.2-slides.html",
+                       output = "slides/1.2-slides.pdf",
                        timeout = 300)
 
 # create png of first slide
-pagedown::chrome_print(input = "https://tradeS23.classes.ryansafner.com/slides/1.1-slides.html",
-                       output = "slides/1.1-slides.png",
+pagedown::chrome_print(input = "https://tradeS23.classes.ryansafner.com/slides/1.2-slides.html",
+                       output = "slides/1.2-slides.png",
                        selector = "div.remark-slide",
                        format = "png",
                        timeout = 300)
