66428da0936c93af4da2c5a419739eb0256d5456
Ryan Safner <ryansafner@gmail.com>
2023-02-07 10:15:08 -0500

    Fix 1.4 practice answer key; 1.5 PDF

--- _site/slides/make-pdfs.R
+++ _site/slides/make-pdfs.R
@@ -2,13 +2,13 @@
 rmarkdown::render("slides/1.1-slides.Rmd")
 
 # make-pdfs
-pagedown::chrome_print(input = "https://tradeS23.classes.ryansafner.com/slides/1.3-slides.html",
-                       output = "slides/1.3-slides.pdf",
+pagedown::chrome_print(input = "https://tradeS23.classes.ryansafner.com/slides/1.5-slides.html",
+                       output = "slides/1.5-slides.pdf",
                        timeout = 300)
 
 # create png of first slide
-pagedown::chrome_print(input = "https://tradeS23.classes.ryansafner.com/slides/1.3-slides.html",
-                       output = "slides/1.3-slides.png",
+pagedown::chrome_print(input = "https://tradeS23.classes.ryansafner.com/slides/1.5-slides.html",
+                       output = "slides/1.5-slides.png",
                        selector = "div.remark-slide",
                        format = "png",
                        timeout = 300)
