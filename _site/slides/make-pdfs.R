db6280d269e778eaffeafba1bf24aac158d032a1
Ryan Safner <ryansafner@gmail.com>
2023-03-23 11:24:29 -0400

    Fix 3.2

--- _site/slides/make-pdfs.R
+++ _site/slides/make-pdfs.R
@@ -2,13 +2,13 @@
 rmarkdown::render("slides/2.3-slides.Rmd")
 
 # make-pdfs
-pagedown::chrome_print(input = "https://tradeS23.classes.ryansafner.com/slides/2.2-slides.html",
-                       output = "slides/2.2-slides.pdf",
+pagedown::chrome_print(input = "https://tradeS23.classes.ryansafner.com/slides/2.3-slides.html",
+                       output = "slides/2.3-slides.pdf",
                        timeout = 300)
 
 # create png of first slide
-pagedown::chrome_print(input = "https://tradeS23.classes.ryansafner.com/slides/2.2-slides.html",
-                       output = "slides/2.2-slides.png",
+pagedown::chrome_print(input = "https://tradeS23.classes.ryansafner.com/slides/2.3-slides.html",
+                       output = "slides/2.3-slides.png",
                        selector = "div.remark-slide",
                        format = "png",
                        timeout = 300)
