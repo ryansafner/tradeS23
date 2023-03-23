5bff04df0134cafb34aee7d187cc5b46fc2b172e
Ryan Safner <ryansafner@gmail.com>
2023-03-23 11:05:16 -0400

    2.3

--- slides/make-pdfs.R
+++ slides/make-pdfs.R
@@ -1,5 +1,5 @@
 # knit Rmd slides to html
-rmarkdown::render("slides/2.1-slides.Rmd")
+rmarkdown::render("slides/2.3-slides.Rmd")
 
 # make-pdfs
 pagedown::chrome_print(input = "https://tradeS23.classes.ryansafner.com/slides/2.2-slides.html",
